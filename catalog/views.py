38ab3a78b4d5fbb19113636b718687a036cddb02
PavelShalev <pav089sk@gmail.com>
2026-08-14 20:53:16 +0300

    create html pages and their render

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, "catalog/home.html")
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name)
+        print(phone)
+        print(message)
+        return HttpResponse(f"Благодарим Вас, {name}! Ваш телефон - {phone}\n"
+                            f"Ваше сообщение получено.")
+    return render(request, "catalog/contacts.html")
