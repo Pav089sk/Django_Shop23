cccd22e71218cdb13093cb688167c3816c6bd1a4
PavelShalev <pav089sk@gmail.com>
2026-08-23 14:45:19 +0300

    create task3 make megrate

--- catalog/models.py
+++ catalog/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['name']
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
@@ -20,10 +21,9 @@ class Product(models.Model):
         verbose_name='Категория')
 
     price = models.DecimalField(max_digits=10, decimal_places=2,
-                              default='0.00', verbose_name="Цена",
-                              help_text="Цена в рублях, с точностью до копеек")
-    created_at = models.DateField(verbose_name='Дата создания')
-    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения')
+                              default='0.00', verbose_name="Цена")
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
     def __str__(self):
         return f'{self.name} {self.category} {self.price}'
