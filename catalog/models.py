90e7f4b719fb4faf1646b5bbd0a4d0c7f8c0aa15
PavelShalev <pav089sk@gmail.com>
2026-08-23 18:57:01 +0300

    create task5

--- catalog/models.py
+++ catalog/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description= models.CharField(max_length=250, verbose_name='Описание')
-    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    image = models.ImageField(upload_to='images/',null=True, blank=True, verbose_name='Изображение')
     category = models.ForeignKey(Category,
         on_delete=models.PROTECT,
         verbose_name='Категория')
