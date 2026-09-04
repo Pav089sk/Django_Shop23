8b57053e0f89fe280c3b4c5552b1358c5f60f54c
PavelShalev <pav089sk@gmail.com>
2026-09-04 21:32:31 +0300

    task 25-2 created templates

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('article/new/', ArticleCreateView.as_view(), name='article_form'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
-    path('article/', ArticleListView.as_view(), name='article_list'),
+    path('articles/', ArticleListView.as_view(), name='article_list'),
     path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
