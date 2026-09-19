d41e338326876c6927b605f37ecf0934dfba82f7
PavelShalev <pav089sk@gmail.com>
2026-09-19 17:50:55 +0300

    redis install

--- config/settings.py
+++ config/settings.py
@@ -167,3 +167,11 @@ LOGIN_URL = 'users:login'
 # EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
